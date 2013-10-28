eb655bd3c214e71d5506ff8e90bd570d14a83660
Ho-Cheung Derek Lai <laihoche@mathlab.utsc.utoronto.ca>
2013-10-28 14:56:10 -0400

    added more code

--- AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/Assignment_view.java
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/Assignment_view.java
@@ -1,5 +1,22 @@
 package com.example.minutemadeproject;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
-public class Assignment_view {
-
+public class Assignment_view extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.assignment);
+         
+        TextView txtAssignment = (TextView) findViewById(R.id.item);
+         
+        Intent i = getIntent();
+        // getting attached intent data
+        String assignment = i.getStringExtra("assignment");
+        // displaying selected product name
+        txtAssignment.setText(assignment);
+         
+    }
 }
