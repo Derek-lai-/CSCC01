dea341c997b7ff8f63a7071710072a3a2505e139
David Kua <david@kua.io>
2013-10-29 16:05:16 -0400

    Added DatabaseManager to help db connections.

---
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/DatabaseManager.java
@@ -0,0 +1,27 @@
+package com.example.minutemadeproject;
+
+import android.content.Context;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+
+public class DatabaseManager {
+
+    private DatabaseHelper databaseHelper = null;
+
+    // Checks and returns a DatabaseHelper if it exists, else returns a fresh one.
+    public DatabaseHelper getDatabaseHelper(Context context) {
+        if (databaseHelper == null) {
+            databaseHelper = OpenHelperManager.getHelper(context, DatabaseHelper.class);
+        }
+        return databaseHelper;
+    }
+
+    // Releases the current DatabaseHelper once usage is done.
+    public void releaseDatabaseHelper(DatabaseHelper databaseHelper) {
+        if (databaseHelper != null) {
+            OpenHelperManager.releaseHelper();
+            databaseHelper = null;
+        }
+    }
+}
