e5f3a0ec5bc2f7c4746cfa8516e30a9248f17814
Derek-Lai- <hocheung.lai@mail.utoronto.ca>
2013-11-01 15:51:32 -0400

    beginning of new framework for assignment (redo)

--- AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/models/Assignment.java
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/models/Assignment.java
@@ -5,6 +5,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
 
+<<<<<<< HEAD
 import java.util.Date;
 
 
@@ -48,4 +49,46 @@ public class Assignment {
     public String toString() {
         return name + "for " + course.name + "due " + dueDate;
     }
+=======
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class Assignment extends ListActivity{
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		//gets xml resources and stores into array
+		String[] assignments = getResources().getStringArray(R.array.ass_ignment);
+		
+		//Binds resource array to ListAdapter
+		this.setListAdapter(new ArrayAdapter<String>(this, R.layout.assignment_list, R.id.item, assignments));
+		
+		ListView lv = getListView();
+		
+		//listening for list item click
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				
+				// selected item
+				String agmt = ((TextView) view).getText().toString();
+				
+				//Launch new Activity on selecting List item
+				Intent i = new Intent(getApplicationContext(), Assignment_view.class);
+				//send data to new activity
+				i.putExtra("assignment", agmt);
+				startActivity(i);
+				
+			}
+		});
+	}
+>>>>>>> beginning of new framework for assignment (redo)
 }
