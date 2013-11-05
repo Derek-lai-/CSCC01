4adfec8780997e3e498c320235ba307a780f2ffa
David Kua <david@kua.io>
2013-11-05 12:28:01 -0500

    Added helpers for Course, Instructor, and Tutorial

---
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/helpers/InstructorHelper.java
@@ -0,0 +1,71 @@
+package com.example.minutemadeproject.helpers;
+
+import android.content.Context;
+
+import com.example.minutemadeproject.models.Instructor;
+import com.example.minutemadeproject.db.DatabaseHelper;
+import com.example.minutemadeproject.db.DatabaseManager;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class InstructorHelper {
+    private DatabaseHelper db;
+    Dao<Instructor, Integer> instructorDao;
+
+    public InstructorHelper(Context context) {
+        try {
+            DatabaseManager dbManager = new DatabaseManager();
+            db = dbManager.getHelper(context);
+            instructorDao = db.getInstructorDao();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int create(Instructor instructor) {
+        try {
+            return instructorDao.create(instructor);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int update(Instructor instructor) {
+        try {
+            return instructorDao.update(instructor);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int delete(Instructor instructor) {
+        try {
+            return instructorDao.delete(instructor);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public Instructor get(int id) {
+        try {
+            return instructorDao.queryForId(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List getAll() {
+        try {
+            return instructorDao.queryForAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/helpers/TutorialHelper.java
@@ -0,0 +1,71 @@
+package com.example.minutemadeproject.helpers;
+
+import android.content.Context;
+
+import com.example.minutemadeproject.models.Tutorial;
+import com.example.minutemadeproject.db.DatabaseHelper;
+import com.example.minutemadeproject.db.DatabaseManager;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TutorialHelper {
+    private DatabaseHelper db;
+    Dao<Tutorial, Integer> tutorialDao;
+
+    public TutorialHelper(Context context) {
+        try {
+            DatabaseManager dbManager = new DatabaseManager();
+            db = dbManager.getHelper(context);
+            tutorialDao = db.getTutorialDao();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int create(Tutorial tutorial) {
+        try {
+            return tutorialDao.create(tutorial);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int update(Tutorial tutorial) {
+        try {
+            return tutorialDao.update(tutorial);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int delete(Tutorial tutorial) {
+        try {
+            return tutorialDao.delete(tutorial);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public Tutorial get(int id) {
+        try {
+            return tutorialDao.queryForId(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List getAll() {
+        try {
+            return tutorialDao.queryForAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
