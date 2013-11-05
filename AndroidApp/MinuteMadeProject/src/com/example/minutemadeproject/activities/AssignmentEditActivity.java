87c7aef78c083d721879063180ddda1130c3b3f8
Derek-Lai- <hocheung.lai@mail.utoronto.ca>
2013-11-05 12:18:32 -0500

    Begin coding AssignmentCreate

--- AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/activities/AssignmentEditActivity.java
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/activities/AssignmentEditActivity.java
@@ -1,5 +1,31 @@
 package com.example.minutemadeproject.activities;
 
 
-public class AssignmentEditActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.minutemadeproject.Assignment;
+import com.example.minutemadeproject.AssignmentHelper;
+import com.example.minutemadeproject.R;
+
+public class AssignmentEditActivity extends Activity{
+
+    public AssignmentHelper helper;
+    Assignment assignment = null;
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assignmentcreate);
+
+        Intent i = getIntent();
+        Bundle assignment = getIntent().getExtras();
+        Integer assignmentId;
+        if (assignment != null){
+            assignmentId = assignment.getInt("assignment");
+        }
+
+
+
+    }
 }
