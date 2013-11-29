7b05a6d6aa173695bf84e1620a544f1a298dcf6a
David Xing <davidxing616@gmail.com>
2013-11-29 17:10:47 -0500

    login reimplemented

--- AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/HomeActivity.java
+++ AndroidApp/MinuteMadeProject/src/com/example/minutemadeproject/HomeActivity.java
@@ -1,5 +1,16 @@
 package com.example.minutemadeproject;
 
+import java.util.List;
+
+
+import com.example.minutemadeproject.R;
+import com.example.minutemadeproject.R.id;
+import com.example.minutemadeproject.R.layout;
+import com.example.minutemadeproject.activities.MainmenuActivity;
+import com.example.minutemadeproject.activities.SignupActivity;
+import com.example.minutemadeproject.helpers.InstructorHelper;
+import com.example.minutemadeproject.models.Instructor;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
@@ -9,25 +20,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.minutemadeproject.activities.SignupActivity;
-
 public class HomeActivity extends Activity {
-    Button btnSignIn, btnSignUp, btnDisplayLessonPlan;
-    LoginDataBaseAdapter loginDataBaseAdapter;
-
+    Button btnSignIn, btnSignUp;
+    //LoginDataBaseAdapter loginDataBaseAdapter;
+    InstructorHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        db = new InstructorHelper(getApplicationContext());
         // create a instance of SQLite Database
-        loginDataBaseAdapter = new LoginDataBaseAdapter(this);
-        loginDataBaseAdapter = loginDataBaseAdapter.open();
+       // loginDataBaseAdapter = new LoginDataBaseAdapter(this);
+       // loginDataBaseAdapter = loginDataBaseAdapter.open();
 
         // Get The Refference Of Buttons
         btnSignIn = (Button) findViewById(R.id.buttonSignIN);
         btnSignUp = (Button) findViewById(R.id.buttonSignUP);
-        btnDisplayLessonPlan = (Button) findViewById(R.id.buttonDisplayLessonPlan);
 
         // Set OnClick Listener on SignUp button
         btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -49,8 +57,7 @@ public class HomeActivity extends Activity {
 
         // get the References of views
         final EditText editTextUserName = (EditText) dialog.findViewById(R.id.editTextUserNameToLogin);
-        final EditText editTextPassword = (EditText) dialog.findViewById(R.id.editTextPasswordToLogin);
-
+        final EditText editTextPassword = (EditText) dialog.findViewById(R.id.editTextPasswordToLogin);        
         Button btnSignIn = (Button) dialog.findViewById(R.id.buttonSignIn);
 
         // Set On ClickListener
@@ -60,14 +67,21 @@ public class HomeActivity extends Activity {
                 // get The User name and Password
                 String userName = editTextUserName.getText().toString();
                 String password = editTextPassword.getText().toString();
-
+                final List<Instructor> instructors = db.getAll();
+                String storedPassword="";
+                for(Instructor i: instructors){
+                        if(i.getUsername().equals(userName))
+                                storedPassword = i.getPassword();
+                }
                 // fetch the Password form database for respective user name
-                String storedPassword = loginDataBaseAdapter.getSinlgeEntry(userName);
+                
 
                 // check if the Stored password matches with  Password entered by user
                 if (password.equals(storedPassword)) {
-                    Toast.makeText(HomeActivity.this, "Thank you for logging in " + userName, Toast.LENGTH_LONG).show();
+                    //Toast.makeText(HomeActivity.this, "Thank you for logging in " + userName, Toast.LENGTH_LONG).show();
                     dialog.dismiss();
+                    Intent intent = new Intent(getApplicationContext(), MainmenuActivity.class);
+                    startActivity(intent);
                 } else {
                     Toast.makeText(HomeActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
                 }
@@ -76,17 +90,11 @@ public class HomeActivity extends Activity {
 
         dialog.show();
     }
-    
-    public void displayLessonPlan(View V) {
-                    /// Create Intent for SignUpActivity  abd Start The Activity
-                    Intent intent = new Intent(getApplicationContext(), DisplayLessons.class);
-                    startActivity(intent);
-    }
-    
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // Close The Database
-        loginDataBaseAdapter.close();
+        //loginDataBaseAdapter.close();
     }
 }
